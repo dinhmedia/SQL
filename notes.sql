5659b96e5758978740d47e52edda967b16e8a64b
Bui Van Dinh <40412662+dinhmedia@users.noreply.github.com>
2019-12-11 13:16:39 +0700

    Update notes.sql

--- notes.sql
+++ notes.sql
@@ -1,4 +1,14 @@
-Drop user on database
+- Create user on database and grant privileges in table
+use use dbtest
+go
+create login hddtvt with password='11111';
+create user hddtvt for login hddtvt;
+grant connect to hddtvt;
+grant select on customer to hddtvt;
+grant select, update on echeck to hddtvt;
+grant select, update on halfbill to hddtvt;
+
+- Drop user on database
 use dbtest
 go
 drop user test
